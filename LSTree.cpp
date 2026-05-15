3e64da88f3cf7a486b75de7fde0ee92d9a69ea6f
iwu980 <i.wu980@gmail.com>
2026-05-15 18:48:23 -0400

    Fixed tree building

--- LSTree.cpp
+++ LSTree.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 class LSTree {
     std::unordered_map<int, std::tuple<int, int>> tree_struct;
     //begins with a binary tree where the children of node n are assumed to be the node given by 2n+1 and 2n+2.
@@ -18,8 +19,8 @@ class LSTree {
             int height = std::ceil(std::log2(num_leaves*2));
             nodes_by_height.resize(height);
             for(int i = 0; i < height; i++) {
-                nodes_by_height[i].resize(std::pow(2, height-i-1));
-                for(int j = 0; j < std::pow(2, height-i-1); j++) {
+                nodes_by_height[i].resize(fmin(std::pow(2, height-i-1), num_leaves));
+                for(int j = 0; j < nodes_by_height[i].size(); j++) {
                     nodes_by_height[i][j] = std::pow(2, height-i-1) + j - 1;
                 }
             }
--- greedyls.cpp
+++ greedyls.cpp
@@ -2,5 +2,8 @@
 #include "LSTree.cpp"
 int main(int argc, char* argv[]) {
     LSTree(4);
+    LSTree(7);
+    LSTree(10);
+    LSTree(8);
     return 0;
 }
